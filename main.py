77bd5a9885359a3ef7cb8d3e4c8dd1b4346b0faa
Alex <i@zaharov-1.ru>
2024-07-07 11:00:41 +0500

    Изменение названия переменной

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 bot = telebot.TeleBot(config.API_TOKEN)
 
 
-def data_btc():
+def value_williams():
     btc = yf.Ticker("BTC-USD").history(interval='1h', period="1mo").reset_index()[['Datetime', 'Open', 'Close', 'Low', 'High', 'Volume']]
     btc['Williams'] = WilliamsRIndicator(high=btc['High'], low=btc['Low'], close=btc['Close'], lbp= 14, fillna= False).williams_r()
 
@@ -16,7 +16,7 @@ def data_btc():
 
 
 def telegram_message():
-    value_indicator = data_btc()
+    value_indicator = value_williams()
     if value_indicator < -80:
         bot.send_message(config.CHANNEL_LOGIN, "🟢BTC in the oversold zone")
     elif value_indicator > -80 and value_indicator < -20:
